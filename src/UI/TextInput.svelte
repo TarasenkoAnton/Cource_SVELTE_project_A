5c43264d3f910df581a25c0de34c9901282558d6
netuz <netuzerr@gmail.com>
2020-03-17 00:42:03 +0200

    textInput component

---
+++ src/UI/TextInput.svelte
@@ -0,0 +1,17 @@
+       <script>
+       export let controleType;
+       export let id;
+       export let label;
+       export let rows;
+       export let value;
+       </script>
+
+
+         <div class="form-control">
+            <label for="{id}">{label}}n</label>
+       {#if controleType === 'textarea'}
+            <textarea rows="{rows}}" id="{id}" value={value} on:input/>
+       {:else}
+            <input type="text" id="{id}" bvalue={value} on:input>
+         {/if}
+        </div>
